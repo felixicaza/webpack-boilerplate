8d41344c42705c5ec9fc7c24073ab9e07e67385d
Felix Joliett <fx.joliett17@gmail.com>
2019-06-03 18:13:06 -0600

    Main file

---
+++ src/ts/index.ts
@@ -0,0 +1,15 @@
+// Styles Files
+import(
+   /* webpackMode: "eager" */
+   '../sass/main.scss'
+);
+
+// JavaScript Files
+import(
+   /* webpackMode: "eager" */
+   '@babel/polyfill'
+);
+import(
+   /* webpackMode: "eager" */
+   'modernizr'
+);
